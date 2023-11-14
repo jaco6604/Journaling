70fd8ee358faf105d255be65db5f5337159771d1
jaco6604 <112594925+jaco6604@users.noreply.github.com>
2023-11-14 14:07:55 -0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<inotify.h>
+#include<fstream>
+
+using namespace std;
+string dat;
+int main() {
+	ofstream MyFile("sample.txt");
+
+		MyFile << "This is the first line" << endl;
+		MyFile << "This is the second line" << endl;
+		MyFile << "This is the third line" << endl;
+
+
+
+		cout << "Enter file to track: " << endl;
+		//cin >> dat;
+		dat = ("Sample.txt");
+		if (dat == ("sample.txt")) {
+			ofstream MyFile("j1_sample_text<123>.DAT");
+
+		}
+
+
+
+
+
+
+}
