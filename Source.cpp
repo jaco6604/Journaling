ea937e80f1b628e5e0f132a63cfbeb28d8cfd0db
jaco6604 <112594925+jaco6604@users.noreply.github.com>
2023-11-21 17:29:09 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,21 @@
 #include<iostream>
-#include<inotify.h>
+#include "inotify.h"
 #include<fstream>
+#define IN_ATTRIB
 
 using namespace std;
 string dat;
 int main() {
+
+	struct Journal 
+	{
+		bool addRemove;
+		string timestamp = IN_ATTRIB(utimensat(2));
+		string line;
+
+	};
+
+
 	ofstream MyFile("sample.txt");
 
 		MyFile << "This is the first line" << endl;
@@ -14,12 +25,19 @@ int main() {
 
 
 		cout << "Enter file to track: " << endl;
-		cin >> dat;
+		//cin >> dat;
 		dat = ("Sample.txt");
 		if (dat == ("sample.txt")) {
 			ofstream MyFile("j1_sample_text<123>.DAT");
 
 		}
+
+
+
+
+
+
+}
 
 //for timestamp add/remove and line make a struct for it. these will be contained in the struct. 
 	//create vector for struct as it is easy to manipulate. 
